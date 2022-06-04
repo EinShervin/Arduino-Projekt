84807ceabf515428970a2d63a97816e79a937d95
Shervin Lepinat <shervin.lepinat@bluewin.ch>
2022-06-04 22:17:40 +0200

    feat: switch, set & finish

--- VogelProj.ino/VogelProj.ino.ino
+++ VogelProj.ino/VogelProj.ino.ino
@@ -1,8 +1,10 @@
-int piezo
-int LEDStoppuhr
-int LEDTimer
-int LEDWecker
-int 
+int piezo;
+int LEDStoppuhr;
+int LEDTimer;
+int LEDWecker;
+int wechselBtn;
+int setBtn;
+int beendenBtn;
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,12 +12,18 @@ void setup() {
   LEDWecker = 7;
   LEDTimer = 9;
   LEDStoppuhr = 10;
+  wechselBtn = 1;
+  setBtn = 2;
+  beendenBtn = 3;
+  
   
   pinMode(piezo, OUTPUT);
   pinMode(LEDWecker, OUTPUT);
   pinMode(LEDtimer, OUTPUT);
   pinMode(LEDStoppuhr, OUTPUT);
 }
+
+
 
 void loop() {
   // put your main code here, to run repeatedly:
