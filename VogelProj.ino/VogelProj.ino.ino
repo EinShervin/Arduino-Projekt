940bfe00ce8ba5908582c5f4e38085a7394a93a5
Shervin Lepinat <shervin.lepinat@bluewin.ch>
2022-06-06 12:04:05 +0200

    fix: bugs

--- VogelProj.ino/VogelProj.ino.ino
+++ VogelProj.ino/VogelProj.ino.ino
@@ -15,15 +15,16 @@ int stoppBtn;
 int timeS;
 int timeM;
 
-boolean stand;
+int menu;
+boolean nextMenu;
 
 void setup() {
   // put your setup code here, to run once:
   piezo = 6;
-  ledWecker = 7; 
+  ledWecker = 7;
   ledTimer = 9;
   ledStoppuhr = 10;
-  
+
   pinMode(piezo, OUTPUT);
   pinMode(ledWecker, OUTPUT);
   pinMode(ledTimer, OUTPUT);
@@ -36,76 +37,123 @@ void setup() {
   timeS = 0;
   timeM = 0;
 
-  stand = false;
-  
+  nextMenu = false;
+
+  menu = -1;
+
+  Serial.begin(9600);
   lcd.begin(16, 2);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(millis() / 1000 <= 0){
-    stoppuhr();
+  if (analogRead(menuBtn) > 0 || nextMenu) {
+    menu = menu + 1;
+    if (menu == 3) {
+      menu = 0;
+    }
   }
-  while(analogRead(menuBtn) > 0){
-    if(stand){
-      timer();
-      stand = false;
-    }
-    else{
+
+  switch (menu) {
+    case 0:
+      digitalWrite(ledTimer, LOW);
+      digitalWrite(ledStoppuhr, HIGH);
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("stoppuhr...");
       stoppuhr();
-      stand = true;
-    }
+      break;
+    case 1:
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("wecker...");
+      // wecker();
+      break;
+    case 2:
+      // timer();
+      lcd.clear();
+      lcd.setCursor(0, 1);
+      lcd.print("timer...");
+      break;
   }
 }
 
-void stoppuhr(){
+boolean menuBtnPressed() {
+  return analogRead(menuBtn) != 0;
+}
+
+boolean srtBtnPressed() {
+  return analogRead(setBtn) != 0;
+}
+
+void stoppuhr() {
   lcd.clear();
   lcd.setCursor(0, 1);
   lcd.print("00:00");
-  digitalWrite(ledTimer, LOW);
-  digitalWrite(ledStoppuhr, HIGH);
-  while(analogRead(setBtn) == 0){
-    if(analogRead(menuBtn) != 0){
+  while (!srtBtnPressed()) {
+    if (menuBtnPressed()) {
       return;
     }
   }
   lcd.clear();
-  while(analogRead(stoppBtn) <= 0){
-    String anzeige = "";
-    delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print("loading...");
+  Serial.print(analogRead(stoppBtn));
+  while (analogRead(stoppBtn) <= 0) {
+    for (int i = 0; i < 10; i++) {
+      if (analogRead(stoppBtn) > 0) {
+        timeM = 0;
+        timeS = 0;
+        nextMenu = true;
+        return;
+      } else if (srtBtnPressed()) {
+         
+      }
+      delay(100);
+    }
     lcd.clear();
     lcd.setCursor(0, 1);
     timeS++;
-    if(timeS % 60 == 0){
-      timeM++;
+    if (timeS % 60 == 0) {
+      timeM = timeM + 1;
+      timeS = 0;
     }
-    String sek;
-    String minu;
-    sek = timeS;
-    if(timeS < 10){
-      sek = "0" + timeS;
-      if(timeS == 0){
-        sek = "00"; 
-      }
-    }
-    minu = timeM;
-    if(timeM < 10){
-      minu = "0" + timeM;
-      if(timeM == 0){
-        minu = "00";
-      } 
-    }
-    anzeige = minu + ":" + sek; 
-    lcd.print(anzeige);
+
+    lcd.print(getMin() + ":" + getSek());
   }
   timeM = 0;
   timeS = 0;
+  nextMenu = true;
   lcd.clear();
   lcd.setCursor(0, 1);
   lcd.print("00:00");
 }
 
-void timer(){
+String getSek() {
+  if (timeS < 10) {
+    if (timeS == 0) {
+      return String(0) + String(0); // 0 Sekunden
+    } else {
+      return "0" + String(timeS); // 05 Sekudnen z.B.
+    }
+  } else {
+    return String(timeS);
+  }
+}
+
+String getMin() {
+  if (timeM < 10) {
+    if (timeM == 0) {
+      return String(0) + String(0);
+    } else {
+      return String(0) + timeM;
+    }
+  } else {
+    return String(timeM);
+  }
+}
+
+void timer() {
   digitalWrite(ledStoppuhr, LOW);
   digitalWrite(ledTimer, HIGH);
   lcd.setCursor(0, 0);
